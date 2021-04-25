f3739fe8bf6b9048ecc434b5a128390bc96b63e2
Sergey Novikov <foxinazombobombo@mail.ru>
2021-04-25 20:15:06 +0300

    Rename the mock file for the film card

--- src/mock/fake-data-film-card.js
+++ src/mock/fake-data-film-card.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { getRandomInteger, isActive } from '../utils.js';
 
 
 // ----------- CONSTANTS -----------
@@ -12,23 +13,14 @@ const MIN_RATE = 0;
 const MAX_RATE = 10;
 const SYMBOLS_COUNT_AFTER_COMMA = 1;
 
+const MIN_MANUFACTURE_DATE = 1960;
+const MAX_MANUFACTURE_DATE = 2021;
+
 const MAX_DAYS_GAP = 7;
 
 const FIRST_LETTER_INDEX = 0;
 const MAX_DESCRIPTION_LENGTH = 139;
 const ELLIPSIS = '...';
-
-
-/*
- * *** Util function to generate a random number from a range
- * *** Source: https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
- */
-const getRandomInteger = (a = 0, b = 1) => {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
 
 
 /*
@@ -77,9 +69,17 @@ const getFilmRate = () => {
 
 
 /*
- * *** Function to generate a film manufacture date (randomized with an error ± 7 days)
+ * *** Function to generate a film manufacture date
  */
 const getFilmManufactureDate = () => {
+  return getRandomInteger(MIN_MANUFACTURE_DATE, MAX_MANUFACTURE_DATE);
+};
+
+
+/*
+ * *** Function to generate a film duration (randomized with an error ±7 days)
+ */
+const getFilmDuration = () => {
   const daysGap = getRandomInteger(-MAX_DAYS_GAP, MAX_DAYS_GAP);
   return dayjs().add(daysGap, 'day').toDate();
 };
@@ -143,16 +143,15 @@ const generateFilmGenre = () => {
 
 
 export const generateFilmCard = () => {
-  const filmRate = getFilmRate();
-  const filmManufactureDate = getFilmManufactureDate();
-
-
   return {
     poster: getFilmPoster(),
+    inWatchlist: isActive(),
+    alreadyWatched: isActive(),
+    isFavorite: isActive(),
     name: generateFilmName(),
     rate: getFilmRate(),
     manufactureDate: getFilmManufactureDate(),
-    duration: getFilmManufactureDate(),
+    duration: getFilmDuration(),
     genre: generateFilmGenre(),
     description: getFilmDescription(),
     comments: [],
