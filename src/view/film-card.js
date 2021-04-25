3c29c5a19960fe62ed0f43b295fc317fe9ece3df
Sergey Novikov <foxinazombobombo@mail.ru>
2021-04-25 20:26:33 +0300

    Refactor the component code

--- src/view/film-card.js
+++ src/view/film-card.js
@@ -1,9 +1,12 @@
-import dayjs from 'dayjs';
+import { humanizeDate } from '../utils.js'
 
 
 export const createFilmCardTemplate = (filmCard) => {
   const {
     poster,
+    inWatchlist,
+    alreadyWatched,
+    isFavorite,
     name,
     rate,
     manufactureDate,
@@ -13,16 +16,28 @@ export const createFilmCardTemplate = (filmCard) => {
     comments,
   } = filmCard;
 
-  const filmManufactureDate = dayjs(manufactureDate).format('YYYY');
-  const filmDurationHour = dayjs(duration).format('h');
-  const filmDurationMinutes = dayjs(duration).format('mm');
+
+  const wathclistClassName = inWatchlist
+    ? 'film-card__controls-item--active'
+    : '';
+
+  const alreadyWatchedClassName = alreadyWatched
+    ? 'film-card__controls-item--active'
+    : '';
+
+  const favoriteClassName = isFavorite
+    ? 'film-card__controls-item--active'
+    : '';
+
+  const filmDurationHour = humanizeDate(duration, 'h');
+  const filmDurationMinutes = humanizeDate(duration, 'mm');
 
   return `
     <article class="film-card">
       <h3 class="film-card__title">${name}</h3>
       <p class="film-card__rating">${rate}</p>
       <p class="film-card__info">
-        <span class="film-card__year">${filmManufactureDate}</span>
+        <span class="film-card__year">${manufactureDate}</span>
         <span class="film-card__duration">${filmDurationHour}h ${filmDurationMinutes}m</span>
         <span class="film-card__genre">${genre}</span>
       </p>
@@ -30,9 +45,9 @@ export const createFilmCardTemplate = (filmCard) => {
       <p class="film-card__description">${description}</p>
       <a class="film-card__comments">${comments.length} comments</a>
       <div class="film-card__controls">
-        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
-        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched film-card__controls-item--active" type="button">Mark as watched</button>
-        <button class="film-card__controls-item button film-card__controls-item--favorite film-card__controls-item--active" type="button">Mark as favorite</button>
+        <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist ${wathclistClassName}" type="button" title="Add to watchlist">Add to watchlist</button>
+        <button class="film-card__controls-item button film-card__controls-item--mark-as-watched ${alreadyWatchedClassName}" type="button" title="Mark as watched">Mark as watched</button>
+        <button class="film-card__controls-item button film-card__controls-item--favorite ${favoriteClassName}" type="button" title="Mark as favorite">Mark as favorite</button>
       </div>
     </article>
   `;
