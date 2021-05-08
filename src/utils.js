4fea4fcef49a4911a0a05310def377ab80d4c9ff
Sergey Novikov <foxinazombobombo@mail.ru>
2021-05-08 18:21:28 +0300

    Add new functionality to the utility module
    The utility module has been updated.
    
    Added the following functionality:
    ---
    
    * glossary of values for positioning elements ( «RenderPosition» );
    
    * function of drawing an element to the page ( «renderElement()» );
    
    * function for creating a DOM element ( «generateElement()» ).

--- src/utils.js
+++ src/utils.js
@@ -23,3 +23,39 @@ export const isActive = () => {
 export const humanizeDate = (dateValue, dateFormat) => {
   return dayjs(dateValue).format(dateFormat);
 };
+
+
+// *** Glossary for element positions ***
+export const RenderPosition = {
+  BEFOREEND: 'beforeend',
+  AFTEREND: 'afterend',
+};
+
+
+// *** Function for rendering elements ***
+export const renderElement = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+
+    case RenderPosition.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+
+// *** Function for rendering components to site page ***
+export const renderTemplate = (container, template, elementPlace) => {
+  container.insertAdjacentHTML(elementPlace, template);
+};
+
+
+// *** Function for generating DOM-elements ***
+export const generateElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
