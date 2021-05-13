5eecbb1cba225aa89e4bdb7899518a53ee6b3d0d
Sergey Novikov <foxinazombobombo@mail.ru>
2021-05-13 22:30:44 +0300

    Change the rendering logic of the elements
    The logic of rendering elements on the page
    has been adjusted:
    ---
    
    * added a condition for determining the presence
    or absence of movies, based on which the desired
    component is drawn.
    
    * added an additional condition for drawing the
    "Show more" button;
    
    * changed the calculation of the number of movie
    cards remaining to be shown.
    
    A small refactoring of the code was also made:
    the "magic" values were removed, some constants
    were formed in the Glossaries.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import UserRankView from './view/user-rank.js';
 import SiteMenuView from './view/site-menu.js';
 import FilmsSectionView from './view/films-section.js';
 import FilmsListView from './view/films-list.js';
+import EmptyFilmsListView from './view/films-list-empty.js';
 import FilmCardView from './view/film-card.js';
 import ShowMoreButtonView from './view/button-show-more.js';
 import FilmAboutPopup from './view/popup-about-film.js';
@@ -16,11 +17,14 @@ import { renderElement, RenderPosition } from './utils.js';
 const FIRST_ARRAY_ELEMENT = 0;
 const ZERO_LENGTH = 0;
 
-const FILMS_COUNT = 17;
-const MAX_SHOWN_FILMS_COUNT = 5;
-const EXTRA_FILMS_COUNT = 2;
-
 const DISPLAY_NONE = 'display: none';
+
+// *** Glossary for films count ***
+const FilmsCount = {
+  TOTAL: 17,
+  MAX_SHOWN: 5,
+  MAX_SHOWN_IN_EXTRA_LIST: 2,
+};
 
 // *** Glossary for films list sections ***
 const FilmsListSection = {
@@ -39,6 +43,12 @@ const FilmsListSection = {
   },
 };
 
+// *** Glossary for «Escape» key properties ***
+const EscapeKey = {
+  FULL_NAME: 'Escape',
+  ABBREVIATED_NAME: 'Esc',
+};
+
 
 // ---------- Main DOM-elements ---------
 const body = document.querySelector('body');
@@ -54,8 +64,15 @@ const siteMainContent = body.querySelector('.main');
 
 
 // *** Array with temporary film cards ***
-const filmCards = new Array(FILMS_COUNT).fill().map(generateFilmCard);
+const filmCards = new Array(FilmsCount.TOTAL).fill().map(generateFilmCard);
 const siteMenuFilters = generateSiteMenu(filmCards);
+
+/*
+ * --- We need to copy the original array of cards
+ * --- in order to perform various manipulations with it
+ * --- (for example, deleting elements) without changing it.
+ */
+const copyOfFilmCardsArray = filmCards.slice();
 
 
 /*
@@ -78,115 +95,142 @@ renderElement(siteMainContent, new FilmsSectionView().getElement(), RenderPositi
  * 3) template of «Most commented»;
  */
 const filmsSection = siteMainContent.querySelector('.films');
-renderElement(filmsSection, new FilmsListView(FilmsListSection.allMovies.id, FilmsListSection.allMovies.title).getElement(), RenderPosition.BEFOREEND);
-renderElement(filmsSection, new FilmsListView(FilmsListSection.topRated.id, FilmsListSection.topRated.title).getElement(), RenderPosition.BEFOREEND);
-renderElement(filmsSection, new FilmsListView(FilmsListSection.mostCommented.id, FilmsListSection.mostCommented.title).getElement(), RenderPosition.BEFOREEND);
 
+if (filmCards.length === ZERO_LENGTH) {
+  renderElement(filmsSection, new EmptyFilmsListView().getElement(), RenderPosition.BEFOREEND);
+} else {
+  renderElement(filmsSection, new FilmsListView(FilmsListSection.allMovies.id, FilmsListSection.allMovies.title).getElement(), RenderPosition.BEFOREEND);
+  renderElement(filmsSection, new FilmsListView(FilmsListSection.topRated.id, FilmsListSection.topRated.title).getElement(), RenderPosition.BEFOREEND);
+  renderElement(filmsSection, new FilmsListView(FilmsListSection.mostCommented.id, FilmsListSection.mostCommented.title).getElement(), RenderPosition.BEFOREEND);
 
-/*
- * «All movies» list items rendering
- */
-const filmsListAll = filmsSection.querySelector('#all-movies');
-const filmsListAllContainer = filmsListAll.querySelector('.films-list__container');
-
-
-// *** Handler for Film card elements click event ***
-function onFilmCardClick (currentFilmCard) {
-  const filmAboutPopupElement = new FilmAboutPopup(currentFilmCard).getElement();
-  const filmAboutPopupCloseButton = filmAboutPopupElement.querySelector('#film-details-close-btn');
-
-  // --- Handler for Popup close button click event ---
-  function onCloseButtonClick (closeEvt) {
-    closeEvt.preventDefault();
-
-    body.removeChild(filmAboutPopupElement);
-    body.classList.remove('hide-overflow');
-    filmAboutPopupCloseButton.removeEventListener('click', onCloseButtonClick);
-  }
-
-  filmAboutPopupCloseButton.addEventListener('click', onCloseButtonClick);
 
   /*
-   * Film description popup rendering
+   * «All movies» list items rendering
    */
-  body.appendChild(filmAboutPopupElement);
-  body.classList.add('hide-overflow');
-}
+  const filmsListAll = filmsSection.querySelector('#all-movies');
+  const filmsListAllContainer = filmsListAll.querySelector('.films-list__container');
 
 
-// *** Function of rendering a film card ***
-function renderFilmCard (cardsContainer, card) {
-  const filmCardComponent = new FilmCardView(card);
+  // *** Handler for Film card elements click event ***
+  const onFilmCardClick = (currentFilmCard) => {
+    const filmAboutPopupElement = new FilmAboutPopup(currentFilmCard).getElement();
+    const filmAboutPopupCloseButton = filmAboutPopupElement.querySelector('#film-details-close-btn');
 
-  renderElement(cardsContainer, filmCardComponent.getElement(), RenderPosition.BEFOREEND);
+    // --- Handler for «Escape» keydown event ---
+    const onEscPress = (escPressEvt) => {
+      escPressEvt.preventDefault();
 
-  const filmCardPoster = filmCardComponent.getElement().querySelector('.film-card__poster');
-  const filmCardTitle = filmCardComponent.getElement().querySelector('.film-card__title');
-  const filmCardComments = filmCardComponent.getElement().querySelector('.film-card__comments');
+      if (escPressEvt.key === EscapeKey.FULL_NAME || escPressEvt.key === EscapeKey.ABBREVIATED_NAME) {
+        body.removeChild(filmAboutPopupElement);
+        body.classList.remove('hide-overflow');
+        filmAboutPopupCloseButton.removeEventListener('click', onCloseButtonClick);
+        document.removeEventListener('keydown', onEscPress);
+      }
+    };
 
-  filmCardPoster.addEventListener('click', () => {
-    onFilmCardClick(card);
-  });
+    // --- Handler for Popup close button click event ---
+    const onCloseButtonClick = (closeEvt) => {
+      closeEvt.preventDefault();
 
-  filmCardTitle.addEventListener('click', () => {
-    onFilmCardClick(card);
-  });
+      body.removeChild(filmAboutPopupElement);
+      body.classList.remove('hide-overflow');
+      filmAboutPopupCloseButton.removeEventListener('click', onCloseButtonClick);
+      document.removeEventListener('keydown', onEscPress);
+    };
 
-  filmCardComments.addEventListener('click', () => {
-    onFilmCardClick(card);
-  });
-}
+    filmAboutPopupCloseButton.addEventListener('click', onCloseButtonClick);
+    document.addEventListener('keydown', onEscPress);
 
-
-// *** Function of rendering the remaining film cards ***
-function remainingFilmCardsRendering () {
-  // --- Count of remaining film cards ---
-  const remainingCardsCount = filmCards.length < MAX_SHOWN_FILMS_COUNT
-    ? filmCards.length
-    : MAX_SHOWN_FILMS_COUNT;
-
-
-  for (let i = 0; i < remainingCardsCount; i++) {
-    renderFilmCard(filmsListAllContainer, filmCards[i]);
-  }
-
-  filmCards.splice(FIRST_ARRAY_ELEMENT, remainingCardsCount);
-}
-
-// *** First call of function for film cards rendering ***
-remainingFilmCardsRendering();
+    /*
+     * Film description popup rendering
+     */
+    body.appendChild(filmAboutPopupElement);
+    body.classList.add('hide-overflow');
+  };
 
 
-// *** Rendering of «Show more» button ***
-renderElement(filmsListAll, new ShowMoreButtonView().getElement(), RenderPosition.BEFOREEND);
+  // *** Function of rendering a film card ***
+  const renderFilmCard = (cardsContainer, card) => {
+    const filmCardComponent = new FilmCardView(card);
+
+    renderElement(cardsContainer, filmCardComponent.getElement(), RenderPosition.BEFOREEND);
+
+    const filmCardPoster = filmCardComponent.getElement().querySelector('.film-card__poster');
+    const filmCardTitle = filmCardComponent.getElement().querySelector('.film-card__title');
+    const filmCardComments = filmCardComponent.getElement().querySelector('.film-card__comments');
+
+    filmCardPoster.addEventListener('click', () => {
+      onFilmCardClick(card);
+    });
+
+    filmCardTitle.addEventListener('click', () => {
+      onFilmCardClick(card);
+    });
+
+    filmCardComments.addEventListener('click', () => {
+      onFilmCardClick(card);
+    });
+  };
 
 
-/*
- * «Top rated» and «Most commented» lists items rendering
- */
-const filmsListTopRated = filmsSection.querySelector('#top-rated .films-list__container');
-const filmsListMostCommented = filmsSection.querySelector('#most-commented .films-list__container');
-for (let i = 0; i < EXTRA_FILMS_COUNT; i++) {
-  renderFilmCard(filmsListTopRated, filmCards[i]);
-  renderFilmCard(filmsListMostCommented, filmCards[i]);
-}
+  // *** Function of rendering the remaining film cards ***
+  const remainingFilmCardsRendering = () => {
+    // --- Count of remaining film cards ---
+    const remainingCardsCount = copyOfFilmCardsArray.length < FilmsCount.MAX_SHOWN
+      ? copyOfFilmCardsArray.length
+      : FilmsCount.MAX_SHOWN;
 
 
-/*
- * *** The logic of displaying additional movie cards when you click on the "Show more" button ***
- */
-const showMoreButton = document.querySelector('#show-more-button');
+    for (let i = 0; i < remainingCardsCount; i++) {
+      renderFilmCard(filmsListAllContainer, copyOfFilmCardsArray[i]);
+    }
 
-// *** Function for event listener of "Show more" button ***
-function showMoreFilmCards (clickEvt) {
-  clickEvt.preventDefault();
+    copyOfFilmCardsArray.splice(FIRST_ARRAY_ELEMENT, remainingCardsCount);
+  };
 
+  // *** First call of function for film cards rendering ***
   remainingFilmCardsRendering();
 
-  if (filmCards.length === ZERO_LENGTH) {
-    showMoreButton.style = DISPLAY_NONE;
-    showMoreButton.removeEventListener('click', showMoreFilmCards);
+
+  /*
+   * *** Rendering of «Show more» button ***
+   */
+  if (filmCards.length > FilmsCount.MAX_SHOWN) {
+    renderElement(filmsListAll, new ShowMoreButtonView().getElement(), RenderPosition.BEFOREEND);
+
+    /*
+     * *** The logic of displaying additional movie cards when you click on the "Show more" button ***
+     */
+    const showMoreButton = document.querySelector('#show-more-button');
+
+    // *** Function for event listener of "Show more" button ***
+    const showMoreFilmCards = (clickEvt) => {
+      clickEvt.preventDefault();
+
+      remainingFilmCardsRendering();
+
+      if (copyOfFilmCardsArray.length === ZERO_LENGTH) {
+        showMoreButton.style = DISPLAY_NONE;
+        showMoreButton.removeEventListener('click', showMoreFilmCards);
+      }
+    };
+
+    showMoreButton.addEventListener('click', showMoreFilmCards);
+  }
+
+
+  /*
+   * «Top rated» and «Most commented» lists items rendering
+   */
+  const filmsListTopRated = filmsSection.querySelector('#top-rated .films-list__container');
+  const filmsListMostCommented = filmsSection.querySelector('#most-commented .films-list__container');
+
+  const maxShownExtraFilmsCount = filmCards.length < FilmsCount.MAX_SHOWN_IN_EXTRA_LIST
+    ? filmCards.length
+    : FilmsCount.MAX_SHOWN_IN_EXTRA_LIST;
+
+  for (let i = 0; i < maxShownExtraFilmsCount; i++) {
+    renderFilmCard(filmsListTopRated, filmCards[i]);
+    renderFilmCard(filmsListMostCommented, filmCards[i]);
   }
 }
-
-showMoreButton.addEventListener('click', showMoreFilmCards);
