cf51208f8103be80d303f97cb79b4139e22f1cf4
Sergey Novikov <foxinazombobombo@mail.ru>
2021-05-20 01:17:10 +0300

    Add event subscriptions
    Event subscriptions have been implemented
    inside components.

--- src/view/button-show-more.js
+++ src/view/button-show-more.js
@@ -8,7 +8,32 @@ const createButtonShowMoreTemplate = () => {
 
 
 export default class ShowMoreButton extends AbstractView {
+  constructor () {
+    super();
+
+    this._clickHandler = this._clickHandler.bind(this);
+  }
+
+
   getTemplate () {
     return createButtonShowMoreTemplate();
+  }
+
+
+  _clickHandler (evt) {
+    evt.preventDefault();
+    this._callback.click();
+  }
+
+
+  addClickHandler (callbackFunction) {
+    this._callback.click = callbackFunction;
+    this.getElement().addEventListener('click', this._clickHandler);
+  }
+
+
+  removeClickHandler (callbackFunction) {
+    this._callback.click = callbackFunction;
+    this.getElement().removeEventListener('click', this._clickHandler);
   }
 }
--- src/view/popup-about-film.js
+++ src/view/popup-about-film.js
@@ -199,10 +199,29 @@ export default class FilmAboutPopup extends AbstractView {
 
     this._filmCardElement = filmCardElement;
     this._element = null;
+    this._clickHandler = this._clickHandler.bind(this);
   }
 
 
   getTemplate () {
     return createPopupAboutFilmTemplate(this._filmCardElement);
+  }
+
+
+  _clickHandler (evt) {
+    evt.preventDefault();
+    this._callback.click();
+  }
+
+
+  addCloseButtonClickHandler (callbackFunction) {
+    this._callback.click = callbackFunction;
+    this.getElement().querySelector('#film-details-close-btn').addEventListener('click', this._clickHandler);
+  }
+
+
+  removeCloseButtonClickHandler (callbackFunction) {
+    this._callback.click = callbackFunction;
+    this.getElement().querySelector('#film-details-close-btn').removeEventListener('click', this._clickHandler);
   }
 }
